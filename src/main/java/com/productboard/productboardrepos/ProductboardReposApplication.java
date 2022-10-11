9c8c946720cc9bddbb9a67353c731dd83436e5f2
vachuda.jan <vachuda.jan@ifortuna.cz>
2022-10-11 06:53:47 +0200

    Initial commit

---
+++ src/main/java/com/productboard/productboardrepos/ProductboardReposApplication.java
@@ -0,0 +1,13 @@
+package com.productboard.productboardrepos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductboardReposApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductboardReposApplication.class, args);
+    }
+
+}
